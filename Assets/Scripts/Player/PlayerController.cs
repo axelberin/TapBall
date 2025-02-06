dd2c98ce938da8e119417bbdad4a862689d63fa2
axelberin <axel.berin.2000@gmail.com>
2025-02-06 10:58:03 -0300

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -80,7 +80,6 @@ public class PlayerController : MonoBehaviour, IPauseble, ISkinLoader
         else
             dirX = Mathf.Min(dir.x, 0.2f);
 
-        Debug.Log(dirX);
         dir = new Vector3(dirX, dir.y < 0 ? -0.2f : 0.2f, dir.z);
         _rb.AddForce(dir * _jumpForce, ForceMode2D.Impulse);
     }
