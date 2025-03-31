a43bfc3975e5d8cd6889f47ff21413943c114a6c
axelberin <axel.berin.2000@gmail.com>
2025-03-31 18:33:39 -0300

    fix

--- Assets/Scripts/Objects/WorldStateController.cs
+++ Assets/Scripts/Objects/WorldStateController.cs
@@ -68,7 +68,7 @@ public class WorldStateController : MonoBehaviour, IPauseble
 
     private void Update()
     {
-        if (TutorialManager.Instance.GetInTutorial)
+        if (TutorialManager.Instance && TutorialManager.Instance.GetInTutorial)
             return;
 
         OnUpdate?.Invoke();
