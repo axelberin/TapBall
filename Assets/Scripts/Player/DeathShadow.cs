d84f8d164ba45e63c4be1bd020fb7332d94523fc
axelberin <axel.berin.2000@gmail.com>
2025-04-21 11:38:48 -0300

    magama ball

--- Assets/Scripts/Player/DeathShadow.cs
+++ Assets/Scripts/Player/DeathShadow.cs
@@ -5,14 +5,12 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 public class DeathShadow : MonoBehaviour, ISkinLoader
 {
     private SpriteRenderer _spriteRenderer;
-    private ParticleSystem _particleSystem;
 
     private void Start()
     {
         transform.localScale = GameManager.Instance.SetGetPlayer.transform.localScale;
 
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _particleSystem = GetComponent<ParticleSystem>();
 
         if (_spriteRenderer != null)
             Addressables.LoadAssetAsync<GameObject>("Death" + SaveAndLoadManager.GetStringValue(
@@ -24,7 +22,11 @@ public class DeathShadow : MonoBehaviour, ISkinLoader
     public void OnPrefabLoaded(AsyncOperationHandle<GameObject> handle)
     {
         if (handle.Status == AsyncOperationStatus.Succeeded)
-            _spriteRenderer.sprite = handle.Result.GetComponent<SpriteRenderer>().sprite;
+        {
+            var handleRenderer = handle.Result.GetComponent<SpriteRenderer>();
+            _spriteRenderer.sprite = handleRenderer.sprite;
+            _spriteRenderer.color = handleRenderer.color;
+        }
         else
             Debug.LogError("Failed to load prefab.");
     }
