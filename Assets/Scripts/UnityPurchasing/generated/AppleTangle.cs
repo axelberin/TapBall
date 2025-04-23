c7aecae43216cc05a7b86c1528fba75d92869865
axelberin <axel.berin.2000@gmail.com>
2025-04-23 19:16:26 -0300

    clave generada

---
+++ Assets/Scripts/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,18 @@
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("Mnj/F2Je6INH9cO4VC6ydfRz50SavJiKj9mIj5+Bzfz84Oms3uPj+AyYp1zlyxj6hXJ45wGizCp7y8Hz880kFHVdRuoQqOedXC83aJemT5MHlQVSdcfgeYsnrryOZJSydNyFX6zPzbwOja68gYqFpgrECnuBjY2Nub69uLy/utabgb+5vL68tb69uLz2vA6N+ryCio/ZkYONjXOIiI+OjZMJDwmXFbHLu34lF8wCoFg9HJ5U9azt///54en/rO3v7+n8+O3i7+mBioWmCsQKe4GNjYmJjI8OjY2M0Pjl6uXv7fjprO71rO3i9az87f74O5cxH86onqZLg5E6wRDS70THDJu8nYqP2YiGn4bN/Pzg6azF4u+ivTm2IXiDgowehz2tmqL4WbCBV+6asarrrAa/5nuBDkNSZy+jdd/m1+je6eDl7eLv6azj4qz45OX/rO/p/kWV/nnRglnz0xd+qY822QPB0YF9+OTj/uX49b2avJiKj9mIj5+BzfymCsQKe4GNjYmJjLzuvYe8hYqP2RkS9oAoywfXWJq7v0dIg8FCmOVdPbzUYNaIvgDkPwORUun/c+vS6TCIip+O2d+9n7ydio/ZiIafhs38/KLMKnvLwfOE0ryTio/Zka+IlLya7uDprP/47eLo7f7orPjp/uH/rO2THVeSy9xniWHS9QihZ7ou28DZYIMRsX+nxaSWRHJCOTWCVdKQWkexoKzv6f745erl7+346az84+Dl7/X+7e/45e/prP/47fjp4eni+P+ivCRQ8q65RqlZVYNa51guqK+dey0g5erl7+345ePirM35+OTj/uX49b3VK4mF8JvM2p2S+F87B6+3yy9Z44tg8bUPB9+sX7RIPTMWw4bnc6dw+/ui7fz84Omi7+Pho+38/ODp7+26FcCh9DthABdQf/sXfvpe+7zDTazj6qz45Oms+OTp4qzt/Pzg5e/t6LmvmceZ1ZE/GHt6EBJD3DZN1NyE0rwOjZ2Kj9mRrIgOjYS8Do2IvISnio2JiYuOjZqS5Pj4/P+2o6P7VbrzTQvZVSsVNb7Od1RZ/RLyLd4D/w3sSpfXhaMePnTIxHzstBKZefzg6aze4+P4rM/NvJKbgby6vLi+io/ZkYKImoiYp1zlyxj6hXJ45wEOjYyKhaYKxAp77+iJjbwNfrymiqq8qIqP2YiHn5HN/Pzg6azP6f74yfKTwOfcGs0FSPjuh5wPzQu/Bg2/uta87r2HvIWKj9mIip+O2d+9n/zg6azP6f745erl7+345ePirM35vA6IN7wOjy8sj46Njo6NjryBioVM77/7e7aLoNpnVoOtglY2/5XDOeLorO/j4ujl+OXj4v+s4+qs+f/piryDio/ZkZ+NjXOIibyPjY1zvJGjvA1PioSnio2JiYuOjrwNOpYNP4mMjw6Ng4y8Do2Gjg6NjYxoHSWF6wOEOKx7RyCgrOP8OrONvAA7z0MnL/0ey9/ZTSOjzT90d2/8QWovwODprMXi76K9qryoio/ZiIefkc38xVT6E7+Y6S37GEWhjo+NjI0vDo2s7eLorO/p/vjl6uXv7fjl4+Ks/KhuZ107/FODyW2rRn3h9GFrOZub3CYGWVZocFyFi7s8+fmt");
+        private static int[] order = new int[] { 50,12,32,17,16,13,7,28,49,40,47,39,59,40,23,56,38,19,40,59,40,26,32,31,42,56,32,44,46,54,39,52,51,36,48,41,40,40,41,58,55,46,58,48,47,56,49,49,50,57,50,54,55,55,54,58,59,57,59,59,60 };
+        private static int key = 140;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
