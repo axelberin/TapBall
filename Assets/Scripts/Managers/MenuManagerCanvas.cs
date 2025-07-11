1bce5645a8b20dce1054cfb62567739db3383d38
axelberin <axel.berin.2000@gmail.com>
2025-07-11 20:53:26 -0300

    play btn and fixes

--- Assets/Scripts/Managers/MenuManagerCanvas.cs
+++ Assets/Scripts/Managers/MenuManagerCanvas.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 public class MenuManagerCanvas : CanvasElementLocator
 {
     private GameObject _menuPanel;
-    private GameObject _selectModePanel;
+    //private GameObject _selectModePanel;
     private GameObject _dunkLevelsPanel;
 
     void Start()
     {
         Application.targetFrameRate = 60;
 
-        GameManager.Instance.SelectGameMode(0);
+        GameManager.Instance.SelectGameMode(1);     //TODO: Mandar un 0 y luego cambiar el modo desde el menu
 
         _menuPanel = FindAndValidateGameObjectComponent(transform, "MenuPanel");
+        //_selectModePanel = FindAndValidateGameObjectComponent(transform, "SelectModePanel");
         _dunkLevelsPanel = FindAndValidateGameObjectComponent(transform, "DunkLevelsPanel");
-        _selectModePanel = FindAndValidateGameObjectComponent(transform, "SelectModePanel");
+
+        var dunkModeButton = FindAndValidateComponent<Button>(transform, "DunkModeButton");
+        dunkModeButton.onClick.AddListener(() =>
+        {
+            _dunkLevelsPanel.SetActive(true);
+            _menuPanel.SetActive(false);
+            GameManager.Instance.SelectGameMode(1);
+        });
 
         if (!SaveAndLoadManager.ContainsKey(SaveAndLoadManager.CurrentBallSkinName))
         {
@@ -24,33 +32,11 @@ public class MenuManagerCanvas : CanvasElementLocator
             SaveAndLoadManager.SetIntValue(1, SaveAndLoadManager.ObtainedBallSkins + "BallBasicSkin", true);
         }
 
-        var playButton = FindAndValidateComponent<Button>(transform, "PlayBTN");
-        playButton.onClick.AddListener(() =>
-        {
-            _selectModePanel.SetActive(true);
-            _menuPanel.SetActive(false);
-        });
-
-        var dunkModeButton = FindAndValidateComponent<Button>(transform, "DunkModeButton");
-        dunkModeButton.onClick.AddListener(() =>
-        {
-            _dunkLevelsPanel.SetActive(true);
-            _selectModePanel.SetActive(false);
-            GameManager.Instance.SelectGameMode(1);
-        });
-
-        var backModePanelButton = FindAndValidateComponent<Button>(transform, "ModePanelBackButton");
-        backModePanelButton.onClick.AddListener(() =>
-        {
-            _selectModePanel.SetActive(false);
-            _menuPanel.SetActive(true);
-        });
-
         var dunkCloseButton = FindAndValidateComponent<Button>(transform, "DunkCloseButton");
         dunkCloseButton.onClick.AddListener(() =>
         {
             _dunkLevelsPanel.SetActive(false);
-            _selectModePanel.SetActive(true);
+            _menuPanel.SetActive(true);
             GameManager.Instance.SelectGameMode(0);
         });
 
@@ -85,6 +71,8 @@ public class MenuManagerCanvas : CanvasElementLocator
     {
         var fadeAnimator = FindAndValidateGameObjectComponent(transform, "Fade").GetComponent<Animator>();
 
+        int lastDunkLevel = 1;
+
         for (int i = 1; i <= 100; i++)
         {
             var button = FindAndValidateComponent<Button>(transform, $"DunkLevel");
@@ -93,6 +81,8 @@ public class MenuManagerCanvas : CanvasElementLocator
                 break;
 
             button.name = $"DunkLevel{i}";
+            if (SaveAndLoadManager.ContainsKey(SaveAndLoadManager.DunkLevelName + i))
+                lastDunkLevel = SaveAndLoadManager.GetIntValue(SaveAndLoadManager.DunkLevelName + i) + 1;
 
             #region UNLOCK LEVELS
             int levelIndex = i; // Variable temporal para capturar el valor actual de 'i'
@@ -137,6 +127,11 @@ public class MenuManagerCanvas : CanvasElementLocator
             levelNumText.text = i.ToString();
             #endregion
         }
+
+        var playButton = FindAndValidateComponent<Button>(transform, "PlayBTN");
+        playButton.onClick.AddListener(() =>
+            ScenesManager.Instance.LoadSceneAsync((SaveAndLoadManager.DunkLevelName +
+                lastDunkLevel).Replace("_", ""), fadeAnimator));
     }
     #endregion
 }
