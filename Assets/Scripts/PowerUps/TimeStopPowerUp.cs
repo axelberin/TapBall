336341e21b349494154b2d146beab4f942c0acf2
felipehbsk8 <felipehbsk8@gmail.com>
2025-10-30 17:35:40 -0300

    Starting power up implementation

---
+++ Assets/Scripts/PowerUps/TimeStopPowerUp.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TimeStopPowerUp : PowerUp
+{
+    public override void Initialize()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StopPowerUpEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
