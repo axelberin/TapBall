39a3da1f5c6d9ffc50826f41698448e756a54439
axelberin <axel.berin.2000@gmail.com>
2025-12-30 17:51:06 -0300

    UI funtionality

--- Assets/Scripts/UI/StoreCanvas.cs
+++ Assets/Scripts/UI/StoreCanvas.cs
@@ -9,6 +9,14 @@ public class StoreCanvas : CanvasElementLocator
     private TextMeshProUGUI _coinsText;
     private TextMeshProUGUI _orbsText;
     private PopUp _reviewPopUp;
+    private Button _ballsFlapButton;
+    private Image _ballsFlapOnImage;
+    private Image _ballsFlapOffImage;
+    private Button _offersFlapButton;
+    private Image _offersFlapOnImage;
+    private Image _offersFlapOffImage;
+    private GameObject _ballsScroll;
+    private GameObject _offersScroll;
 
     private void Awake()
     {
@@ -29,6 +37,20 @@ public class StoreCanvas : CanvasElementLocator
         closeButton.onClick.AddListener(() => UIManager.Instance.ChangeCanvas(
             "StoreCanvas", "MenuManagerCanvas"));
 
+        _ballsScroll = FindAndValidateGameObjectComponent(transform, "BallSkinsScroll");
+        _offersScroll = FindAndValidateGameObjectComponent(transform, "OffersScroll");
+
+        _ballsFlapButton = FindAndValidateComponent<Button>(transform, "BallsFlapButton");
+        _ballsFlapButton.onClick.AddListener(OnSelectBallsFlap);
+        _ballsFlapOnImage = FindAndValidateComponent<Image>(_ballsFlapButton.transform, "FlapOn");
+        _ballsFlapOffImage = FindAndValidateComponent<Image>(_ballsFlapButton.transform, "FlapOff");
+
+        _offersFlapButton = FindAndValidateComponent<Button>(transform, "OffersFlapButton");
+        _offersFlapButton.onClick.AddListener(OnSelectOffersFlap);
+        _offersFlapOnImage = FindAndValidateComponent<Image>(_offersFlapButton.transform, "FlapOn");
+        _offersFlapOffImage = FindAndValidateComponent<Image>(_offersFlapButton.transform, "FlapOff");
+
+        OnSelectBallsFlap();
         _reviewPopUp = FindAndValidateComponent<PopUp>(transform, "ReviewPopUp");
         _reviewPopUp.gameObject.SetActive(true);
 
@@ -65,5 +87,27 @@ public class StoreCanvas : CanvasElementLocator
     private void OpenReviewFromLink()
     {
         Application.OpenURL("https://play.google.com/store/apps/details?id=" + Application.identifier);
+    }
+
+    private void OnSelectBallsFlap()
+    {
+        _ballsScroll.SetActive(true);
+        _offersScroll.SetActive(false);
+        UpdateFlapsButtonsState();
+    }
+
+    private void OnSelectOffersFlap()
+    {
+        _ballsScroll.SetActive(false);
+        _offersScroll.SetActive(true);
+        UpdateFlapsButtonsState();
+    }
+
+    private void UpdateFlapsButtonsState()
+    {
+        _ballsFlapOffImage.gameObject.SetActive(!_ballsScroll.activeInHierarchy);
+        _ballsFlapOnImage.gameObject.SetActive(_ballsScroll.activeInHierarchy);
+        _offersFlapOffImage.gameObject.SetActive(!_offersScroll.activeInHierarchy);
+        _offersFlapOnImage.gameObject.SetActive(_offersScroll.activeInHierarchy);
     }
 }
